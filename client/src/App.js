ce5d6564e16f827e1c644c5bc27ad42352b65812
Eric <eric@MacBook-Pro.local>
2019-05-06 14:02:21 -0700

    Add in Redux Action/Reducers

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import jwt_decode from 'jwt-decode';
 import './App.css';
+import store from './store';
 
 const App = () => (
-  <Fragment className='App'>
-    {' '}
-    <h1>App</h1>{' '}
-  </Fragment>
+  <Provider store={store}>
+    <Router>
+      <Fragment className='App'>
+        {' '}
+        <h1>App</h1>{' '}
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
